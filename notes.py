7e1184fe74e77282c5248bb18834d4a70c9ac660
Edoardo Negri <edne@gmx.com>
2017-03-20 10:48:33 +0100

    Register user

--- notes.py
+++ notes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from passlib.apps import custom_app_context as pwd_context
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -14,6 +15,48 @@ class Note(db.Model):
     def __iter__(self):
         yield 'id', self.id
         yield 'content', self.content
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), index=True)
+    password_hash = db.Column(db.String(128))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    def __iter__(self):
+        yield 'id', self.id
+        yield 'username', self.username
+
+
+@app.route('/user/<username>')
+def get_user(username):
+    user = User.query.filter_by(username=username).one_or_none()
+    if user:
+        return jsonify(dict(user))
+    else:
+        abort(404)
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.json['username']
+    password = request.json['password']
+
+    if User.query.filter_by(username=username).one_or_none():
+        app.logger.info('Username {} already taken'.format(username))
+        abort(400)
+
+    user = User(username=username)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify(dict(user))
 
 
 @app.route('/add', methods=['POST'])
@@ -30,8 +73,11 @@ def add_note():
 
 @app.route('/note/<int:note_id>')
 def show_note(note_id):
-    note = Note.query.filter_by(id=note_id).first()
-    return jsonify(dict(note))
+    note = Note.query.filter_by(id=note_id).one_or_none()
+    if note:
+        return jsonify(dict(note))
+    else:
+        abort(404)
 
 
 if __name__ == '__main__':
